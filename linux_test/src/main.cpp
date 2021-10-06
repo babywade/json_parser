fbcf9ff8c08f0cf89dbf2d2701f55968b81a89da
babywade <yixiaofei@stu.xjtu.edu.cn>
2021-10-06 23:30:51 +0800

    增加测试CPU大小端程序

--- linux_test/src/main.cpp
+++ linux_test/src/main.cpp
@@ -6,8 +6,39 @@
 
 using namespace std;
 
+void test1();
+void test2_checkCPU();
+int test3_jsonParser();
+
 int main(int argc, char *argv[])
 {
+	// test1();
+	test2_checkCPU();
+	// test3_jsonParser();
+}
+
+void test1() {
+	//测试enum类型的输出
+	TokenType tokenType;
+	tokenType = START_OBJ;
+	cout << tokenType << endl;  //输出0
+	return;
+}
+
+void test2_checkCPU() {
+	//测试 AMD Ryzen 5 1400 Quad-Core Processor 小端
+    union MyUnion{
+        int a; //暂且认为16位，可能short更好
+        char c; //8位
+    }test;
+    test.a = 1; //0x0001
+    if (test.c == 1)
+        cout << "little endian" <<endl;
+    else cout << "big endian" <<endl;
+	return;
+}
+
+int test3_jsonParser() {
 	fstream fs;
 	string line;
 	string str;
@@ -15,7 +46,7 @@ int main(int argc, char *argv[])
 	fs.open("zhihu.txt");
 	if (!fs) {
 		cout << "cannot open the file" << endl;
-		return -1;
+		return 0;
 	}
 	
 	while (getline(fs, line)) {
@@ -33,11 +64,5 @@ int main(int argc, char *argv[])
 	LatestNews latestNewsJSON;
 	Parser(tokenGroup, latestNewsJSON);
 	cout << "finish" << endl;
-}
-
-void test1() {
-	//测试enum类型的输出
-	TokenType tokenType;
-	tokenType = START_OBJ;
-	cout << tokenType << endl;  //输出0
+	return 1;
 }
